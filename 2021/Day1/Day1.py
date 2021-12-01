bca4513af44485fa0e50ebb22358dbb567206714
mringel <mringel@bellevuewa.gov>
2021-12-01 11:19:29 -0800

    minor cleanup

--- 2021/Day1/Day1.py
+++ 2021/Day1/Day1.py
@@ -6,12 +6,7 @@ soundings = []
 for line in lines:
   soundings.append(int(line.rstrip('\n')))
 
-#print(soundings)
-
 answer = 0
-
-# elist = enumerate(soundings)
-
 
 for i in range(len(soundings)-1):
   if soundings[i+1] > soundings[i]:
